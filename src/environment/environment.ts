c8a97296c6af2385078d9ed1dcc94251803c8201
Jean Pires <jean.m.pires@gmail.com>
2023-06-02 19:45:09 -0300

    alterando url dev

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'https://conferidor-de-jogos-production.up.railway.app',
     // outras variáveis de ambiente...
 
 
-    tokenWhitelistedDomains:[ new RegExp('localhost:8080') ],
+    tokenWhitelistedDomains:[ new RegExp('conferidor-de-jogos-production.up.railway.app') ],
     tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')] 
   };
